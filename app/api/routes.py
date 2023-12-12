258ae97dfda5ece9f05b2899be9b70f721607a3b
Donna Vo <you@example.com>
2023-12-11 20:32:47 -0500

    first commit

---
+++ app/api/routes.py
@@ -0,0 +1,67 @@
+from flask import Blueprint, request, jsonify, render_template
+from helpers import token_required
+from models import db, User, Contact, contact_schema, contacts_schema
+
+api = Blueprint('api',__name__, url_prefix='/api')
+
+@api.route('/getdata')
+def getdata():
+    return {'totally': 'tubular'}
+
+@api.route('/contacts', methods = ['POST'])
+@token_required
+def create_contact(current_user_token):
+    first_name = request.json['first_name']
+    last_name = request.json['last_name']
+    email = request.json['email']
+    phone_number = request.json['phone_number']
+    address = request.json['address']
+    user_token = current_user_token.token
+
+    print(f'BIG TESTER: {current_user_token.token}')
+
+    contact = Contact(first_name, last_name, email, phone_number, address, user_token = user_token )
+
+    db.session.add(contact)
+    db.session.commit()
+
+    response = contact_schema.dump(contact)
+    return jsonify(response)
+
+@api.route('/contacts/<id>', methods = ['GET'])
+@token_required
+def get_contact_two(current_user_token, id):
+    fan = current_user_token.token
+    if fan == current_user_token.token:
+        contact = Contact.query.get(id)
+        response = contact_schema.dump(contact)
+        return jsonify(response)
+    else:
+        return jsonify({"message": "Valid Token Required"}),401
+
+# UPDATE endpoint
+@api.route('/contacts/<id>', methods = ['POST','PUT'])
+@token_required
+def update_contact(current_user_token,id):
+    contact = Contact.query.get(id) 
+    contact.first_name = request.json['first_name']
+    contact.last_name = request.json['last_name']
+    contact.email = request.json['email']
+    contact.phone_number = request.json['phone_number']
+    contact.address = request.json['address']
+    contact.user_token = current_user_token.token
+
+    db.session.commit()
+    response = contact_schema.dump(contact)
+    return jsonify(response)
+
+
+# DELETE car ENDPOINT
+@api.route('/contacts/<id>', methods = ['DELETE'])
+@token_required
+def delete_contact(current_user_token, id):
+    contact = Contact.query.get(id)
+    db.session.delete(contact)
+    db.session.commit()
+    response = contact_schema.dump(contact)
+    return jsonify(response)
